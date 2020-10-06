61746c5eabc6dd09c3fbe874285479f123d61d8e
kimlah <wil11070@byui.edu>
2020-10-06 14:55:58 -0700

    Adding a simple file to practice pull/push

---
+++ HelloFromKeeley.java
@@ -0,0 +1,7 @@
+// first Java program for CS 246 on GitHub - Fall 2020
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Keeley.");
+    }
+}
